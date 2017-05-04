b0606a8acb7e287c938e9731315f5a1827724c5f
yaojinwu <350072095@qq.com>
2017-05-04 09:29:28 +0800

    修改图片，关于我们，公司介绍

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 $(function () {
-    $('#code').mousemove(function(e){
-        var x = e.clientX - $(this).offset().left - $('#code-box').width();
-        var y = e.clientY - $(this).offset().top;
-        $('#code-box').show().css({'left':x-10+'px','top':y+10+'px'})
-    }).mouseout(function(){
-        $('#code-box').hide();
-    });
     $(document).scroll(function(){
         if($(document).scrollTop() > 0){
             $('.nav-download').fadeOut();
